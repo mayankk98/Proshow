4d32baaf9c35e64956af0262a94d5117b94b1e86
Mayank Khandelwal <mayankkhandelwal92@gmail.com>
2018-01-16 21:54:07 +0530

    animations added

--- script.js
+++ script.js
@@ -21,4 +21,28 @@ $(document).ready(function() {
             $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
         }, 800);
     });
+
+    $(document).scroll(function(e){
+        e.preventDefault();
+        var current = $('.flex--active').data('slide');
+        $('.slider__wrapper').find('.flex__container[data-slide=' + ((current % 5) + 1) + ']').addClass('flex--preStart');
+        $('.flex--active').addClass('animate--end');
+        setTimeout(function() {
+            $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+            $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+        }, 800);
+        console.log("scroll");
+    })
+    
+    $("#formBtn").on('click', function(e){
+        e.preventDefault();
+       
+        
+        $("img.artist__img").animate({bottom: "-300px"},"slow");
+        $("div.flex__item.flex__item--left").animate({width : 'auto', left : '-1000px'}, "slow");
+        $("div.flex__item.flex__item--right").animate({width : 'auto', right : '-1000px'}, "slow");
+        $("body").css({"background-color": "grey", "opacity": "0.4", "overflow": "none"});
+        console.log("show");
+    });
+   
 });
