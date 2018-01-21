5b5f1508a82f12f28d600da5f040dcd32a619901
arvind0598 <arvind0598@gmail.com>
2018-01-21 12:16:36 +0530

    changed i updation for all functions

--- script.js
+++ script.js
@@ -2,25 +2,28 @@ $(document).ready(function() {
 
     $('#down').on('click', function(e) {
         e.preventDefault();
-        var current = $('.flex--active').data('slide');
-        $('.slider__wrapper').find('.flex__container[data-slide=' + ((current % 5) + 1) + ']').addClass('flex--preStart');
+        var i = $('.flex--active').data('slide');
+        i = (((i + 1) % 5) + 5) % 5;
+        $('.slider__wrapper').find('.flex__container[data-slide=' + i + ']').addClass('flex--preStart');
         $('.flex--active').addClass('animate--end');
         setTimeout(function() {
             $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
             $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
         }, 800);
+        console.log(i);
     });
 
     $('#up').on('click', function(e) {
         e.preventDefault();
-        var current = $('.flex--active').data('slide');
-        next = current == 1 ? 5 : current - 1;
-        $('.slider__wrapper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+        var i = $('.flex--active').data('slide');
+        i = (((i - 1) % 5) + 5) % 5;
+        $('.slider__wrapper').find('.flex__container[data-slide=' + i + ']').addClass('flex--preStart');
         $('.flex--active').addClass('animate--end');
         setTimeout(function() {
             $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
             $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
         }, 800);
+        console.log(i);
     });
 
     $(document).scroll(function(e) {
@@ -97,7 +100,6 @@ $(document).ready(function() {
             $("div.flex__item.flex__item--right").hide();
         });
 
-        $("body").css({ "background-color": "grey", "opacity": "0.4", "overflow": "none" });
-        //console.log("show");
+        //$("body").css({ "background-color": "grey", "opacity": "0.4", "overflow": "none" });
     });
 });
