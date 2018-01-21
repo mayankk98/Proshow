17f3a03900017433b0ea1b99f88785633b8e5dfe
arvind0598 <arvind0598@gmail.com>
2018-01-21 10:07:00 +0530

    add back buttons. handled overflow on register button

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
 
-    // $('#down').on('click', function(e) {
-    //     e.preventDefault();
-    //     var current = $('.flex--active').data('slide');
-    //     $('.slider__wrapper').find('.flex__container[data-slide=' + ((current % 5) + 1) + ']').addClass('flex--preStart');
-    //     $('.flex--active').addClass('animate--end');
-    //     setTimeout(function() {
-    //         $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
-    //         $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
-    //     }, 800);
-    // });
+    $('#down').on('click', function(e) {
+        e.preventDefault();
+        var current = $('.flex--active').data('slide');
+        $('.slider__wrapper').find('.flex__container[data-slide=' + ((current % 5) + 1) + ']').addClass('flex--preStart');
+        $('.flex--active').addClass('animate--end');
+        setTimeout(function() {
+            $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+            $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+        }, 800);
+    });
 
-    // $('#up').on('click', function(e) {
-    //     e.preventDefault();
-    //     var current = $('.flex--active').data('slide');
-    //     next = current == 1 ? 5 : current - 1;
-    //     $('.slider__wrapper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
-    //     $('.flex--active').addClass('animate--end');
-    //     setTimeout(function() {
-    //         $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
-    //         $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
-    //     }, 800);
-    // });
+    $('#up').on('click', function(e) {
+        e.preventDefault();
+        var current = $('.flex--active').data('slide');
+        next = current == 1 ? 5 : current - 1;
+        $('.slider__wrapper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+        $('.flex--active').addClass('animate--end');
+        setTimeout(function() {
+            $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+            $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+        }, 800);
+    });
 
     $(document).scroll(function(e) {
         e.preventDefault();
-        
+
         $('.slider__wrapper').find('.flex__container[data-slide=' + ((current % 5) + 1) + ']').addClass('flex--preStart');
         $('.flex--active').addClass('animate--end');
         setTimeout(function() {
@@ -86,9 +86,17 @@ $(document).ready(function() {
 
     $("#formBtn").on('click', function(e) {
         e.preventDefault();
-        $("img.artist__img").animate({ bottom: "-300px" }, "slow");
-        $("div.flex__item.flex__item--left").animate({ left: '-1000px' }, "slow");
-        $("div.flex__item.flex__item--right").animate({ right: '-1000px' }, "slow");
+        $("img.artist__img").animate({ bottom: "-300px" }, "slow", function() {
+            $("img.artist__img").hide();
+        });
+
+        $("div.flex__item.flex__item--left").animate({ left: '-1000px' }, "slow", function() {
+            $("div.flex__item.flex__item--left").hide();
+        });
+        $("div.flex__item.flex__item--right").animate({ right: '-1000px' }, "slow", function() {
+            $("div.flex__item.flex__item--right").hide();
+        });
+
         $("body").css({ "background-color": "grey", "opacity": "0.4", "overflow": "none" });
         //console.log("show");
     });
